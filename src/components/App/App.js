4e81cc59d4037a128a3b98749e49810ce94c0e5e
2headpepe <st095279@student.spbu.ru>
2023-09-01 13:09:28 +0300

    Project structure

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import Info from "../Info/Info";
+import About from "../About/About";
+import Interests from "../Interests/Interests";
+import Footer from "../Footer/Footer";
+
+function App() {
+  return (
+    <>
+      <Info></Info>
+      <About></About>
+      <Interests></Interests>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
