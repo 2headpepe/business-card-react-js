57c003fcae834575eb1972788f8d85611869c7f1
2headpepe <st095279@student.spbu.ru>
2023-09-02 20:55:59 +0300

    Info ready

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,14 +3,15 @@ import About from "../About/About";
 import Interests from "../Interests/Interests";
 import Footer from "../Footer/Footer";
 
+import "./App.css";
 function App() {
   return (
-    <>
+    <div className="app-wrapper">
       <Info></Info>
       <About></About>
       <Interests></Interests>
       <Footer></Footer>
-    </>
+    </div>
   );
 }
 
--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,7 +1,48 @@
-function Info(){
-    return <>
-        
-    </>
+import Photo from "../../assets/photo.png";
+import Mail from "../../assets/Mail.png";
+import Linkedin from "../../assets/linkedin.png";
+
+import "../Info/Info.css";
+
+function Info() {
+  return (
+    <div className="wrapper">
+      <img
+        className="photo"
+        src={Photo}
+        alt="Photo"
+        width="317px"
+        height="317px"
+      />
+      <h3 className="name">Laura Smith</h3>
+      <h5 className="status">Frontend Developer</h5>
+      <h6 className="website">laurasmith.website</h6>
+      <div className="btn-wrapper">
+        <button
+          className="links-button"
+          id="email-button"
+        >
+          <img
+            src={Mail}
+            alt="mail-icon"
+            className="button-icon"
+          />
+          <span className="button-text">Email</span>
+        </button>
+        <button
+          className="links-button"
+          id="linkedin-button"
+        >
+          <img
+            src={Linkedin}
+            alt="mail-icon"
+            className="button-icon"
+          />
+          <span className="button-text">Linkedin</span>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Info;
