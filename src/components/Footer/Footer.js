264b8a0e84aa33a2bcc815b311051d3a88c2aa0d
2headpepe <st095279@student.spbu.ru>
2023-09-02 21:29:07 +0300

    Footer component is ready

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,34 @@
-function Footer(){
-    return <></>
+import TwitterIcon from "../../assets/twitter-icon.svg";
+import FacebookIcon from "../../assets/facebook-icon.svg";
+import InstagramIcon from "../../assets/instagram-icon.svg";
+import GithubIcon from "../../assets/github-icon.svg";
+
+import "../Footer/Footer.css";
+function Footer() {
+  return (
+    <div className="Footer-wrapper">
+      <img
+        src={TwitterIcon}
+        alt="Twitter"
+        className="Footer-icon"
+      />
+      <img
+        src={FacebookIcon}
+        alt="Facebook"
+        className="Footer-icon"
+      />
+      <img
+        src={InstagramIcon}
+        alt="Instagram"
+        className="Footer-icon"
+      />
+      <img
+        src={GithubIcon}
+        alt="Github"
+        className="Footer-icon"
+      />
+    </div>
+  );
 }
 
 export default Footer;
