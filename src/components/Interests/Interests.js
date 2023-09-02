10880b1d2ef013f3c41e46d6690f041ece3bbac0
2headpepe <st095279@student.spbu.ru>
2023-09-02 21:07:21 +0300

    Interests component is ready

--- src/components/Interests/Interests.js
+++ src/components/Interests/Interests.js
@@ -1,5 +1,15 @@
-function Interests(){
-    return <></>
+import "../Interests/Interests.css";
+
+function Interests() {
+  return (
+    <div className="Interests-wrapper">
+      <h4 className="Interests-header">Interests</h4>
+      <p className="Interests-text">
+        Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+        Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
+      </p>
+    </div>
+  );
 }
 
 export default Interests;
