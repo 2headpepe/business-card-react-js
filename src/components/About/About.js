bc267e2e8d74ec84f352583da324ee063e1135c9
2headpepe <st095279@student.spbu.ru>
2023-09-02 21:02:38 +0300

    About ready

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,16 @@
-function About(){
-    return <></>
+import "../About/About.css";
+
+function About() {
+  return (
+    <div className="About-wrapper">
+      <h4 className="About-header">About</h4>
+      <p className="About-text">
+        I am a frontend developer with a particular interest in making things
+        simple and automating daily tasks. I try to keep up with security and
+        best practices, and am always looking for new things to learn.
+      </p>
+    </div>
+  );
 }
 
 export default About;
